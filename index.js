ebc521c819f9beaab761e24e83e4f8d2dc228e4c
webnnets19 <info@webnnets.com>
2026-05-21 19:07:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -144,4 +144,9 @@ app.get("/", (req, res) => {
     res.send("Backend Running")
 })
 
-export default app
+// server.js
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
