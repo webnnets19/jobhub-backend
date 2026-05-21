682e88c1e3e7e5bb31bfba09a977ce9959995fcb
webnnets19 <info@webnnets.com>
2026-05-21 19:23:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,8 @@ const app = express()
 app.use(cors({
     origin: [
         "https://webnnets.co.in",
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "http://localhost:5174"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
